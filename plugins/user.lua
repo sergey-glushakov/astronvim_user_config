8b75515af9f5e8e2aa2e510465d981a7205a0b7f
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2023-03-30 13:35:29 +0300

    feat: добавлена тема оформления monokai-pro

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,17 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+--     {
+--       "catppuccin/nvim",
+--       as = "catppuccin",
+--       config = function()
+--         require("catppuccin").setup {}
+--       end,
+--     },
+
+    {
+      "loctvl842/monokai-pro.nvim",
+      as = "monokai-pro",
+      config = function()
+        require("monokai-pro").setup {}
+      end,
+    },
 }
